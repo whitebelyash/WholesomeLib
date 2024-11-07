eaa288177b90ad00364defc6207eb65b3c78ba9c
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2024-11-07 20:57:12 +0400

    wlib-lang: improve docs+refactor on LangFile

--- wlib-lang/src/main/java/ru/whbex/lib/lang/LangFile.java
+++ wlib-lang/src/main/java/ru/whbex/lib/lang/LangFile.java
@@ -8,40 +8,67 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-// === Language file ===
-// path.example=Legit
-// path.example = Legit (not supported yet)
-public class LangFile {
+/**
+ * Language file - java.io.File wrapper for .lang files
+ * Format: key.key1.key2=value, key1.key2=   value
+ * Everything after = belongs to value, quotes are not supported yet
+ * TODO: Multiple lines
+ * TODO: Quotes
+ * TODO: spaces between separator
+ * TODO: Unit tests?
+ */
+public final class LangFile {
 
 
     private final File file;
     private BufferedReader reader;
     private String current;
     private boolean open = false;
-
     private int pos = -1;
-
-
-
     private boolean empty = false;
-    // TODO: Separate LangFile instance create and load
-    private final boolean loaded = true;
     public LangFile(File file) {
         LogDebug.print("Creating language file at {0}", file.getAbsolutePath());
         this.empty = !file.exists();
         this.file = file;
     }
+
+    /**
+     * Open language file
+     * @throws IOException on IO errors
+     * @throws UnsupportedOperationException if file doesn't exist
+     */
     public void open() throws IOException {
+        if(empty)
+            throw new UnsupportedOperationException("Language file does not exists, cannot open");
         reader = new BufferedReader(new FileReader(file));
         this.open = true;
         current = reader.readLine();
         pos = 1;
     }
+
+    /**
+     * Close language file. Does nothing if file is already closed.
+     * @throws IOException on IO errors
+     */
     public void close() throws IOException {
+        if(!open)
+            return;
         reader.close();
+        open = false;
     }
+
+    /**
+     * Set language file reader position
+     * @param pos position
+     * @throws IOException on IO errors
+     * @throws UnsupportedOperationException if language file is closed
+     * @throws IllegalArgumentException if position is less than 1
+     */
     public void setPosition(int pos) throws IOException {
-        if(!open) return;
+        if(!open)
+            throw new UnsupportedOperationException("Open language file before using it!");
+        if(pos < 1)
+            throw new IllegalArgumentException("Invalid position!");
         this.pos = pos;
         reader.reset();
         for(int i = 0; i < pos - 1; i++){
@@ -50,21 +77,57 @@ public class LangFile {
         current = reader.readLine();
     }
 
+    /**
+     * Get language file reader position
+     * @return position
+     * @throws UnsupportedOperationException if language file is closed
+     */
+
     public int getPosition() {
+        if(!open)
+            throw new UnsupportedOperationException("Open language file before using it!");
         return pos;
     }
 
+    /**
+     * Check if next line exists in language file
+     * @return check result
+     * @throws IOException on IO errors
+     * @throws UnsupportedOperationException if language file is closed
+     */
     public boolean hasNextLine() throws IOException {
+        if(!open)
+            throw new UnsupportedOperationException("Open language file before using it!");
         return open && current != null;
     }
-    // returns raw string from langfile
+
+    /**
+     * Get full string (line) at current reader position
+     * @return unformatted line (as is)
+     * @throws UnsupportedOperationException if language file is closed
+     */
     public String getCurrentString(){
+        if(!open)
+            throw new UnsupportedOperationException("Open language file before using it!");
         return current;
     }
+
+    /**
+     * Is current string commented
+     * @return check result
+     * @throws UnsupportedOperationException if language file is closed
+     */
     public boolean isCommented(){
+        if(!open)
+            throw new UnsupportedOperationException("Open language file before using it!");
         return current.charAt(0) == '#';
     }
-    // returns split string (where 0 - key, 1 - value) with trimmed comments
+
+    /**
+     * Returns phrase - 2-string array, where s[0] - key, [1] - value
+     * @return array of length 2
+     * @throws UnsupportedOperationException if language file is closed
+     */
     public String[] getCurrentPhrase(){
         if(isCommented())
             return null;
@@ -80,27 +143,42 @@ public class LangFile {
         }
         return ret;
     }
-    // sets current string to next line
+
+    /**
+     * Go to next line in language file
+     * @throws IOException on IO errors
+     * @throws UnsupportedOperationException if language file is closed
+     * TODO: return false if reached file end
+     */
     public void next() throws IOException {
         if(!open)
-            return;
+            throw new UnsupportedOperationException("Open language file before using it!");
         // may be broken
         do {
             pos++;
         } while ((current = reader.readLine()) != null && current.isEmpty());
     }
-    public boolean isStarted(){
+
+    /**
+     * Is language file closed
+     * @return check result
+     */
+    public boolean isClosed(){
         return open;
-
     }
 
-    public boolean isLoaded() {
-        return loaded;
-    }
-    public boolean isEmpty(){
+    /**
+     * Is language file exists
+     * @return check result
+     */
+    public boolean exists(){
         return empty;
     }
 
+    /**
+     * Get IO File that this language file wraps
+     * @return
+     */
     public File getFile() {
         return file;
     }
