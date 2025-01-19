d66ed7346b3e111ad45f2e38acaf2f4fd532a3b0
whbex <66699673+whitebelyash@users.noreply.github.com>
2025-01-19 09:29:09 +0400

    [wlib-sql] ConnectionProvider: doc fixes

--- wlib-sql/src/main/java/ru/whbex/lib/sql/conn/ConnectionProvider.java
+++ wlib-sql/src/main/java/ru/whbex/lib/sql/conn/ConnectionProvider.java
@@ -3,22 +3,25 @@ package ru.whbex.lib.sql.conn;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Single connection pool. Needs testing in a concurrent environment
+ */
 public interface ConnectionProvider {
     String JDBC_PREFIX = "jdbc:";
     /**
-     * Get connection to a DB. Returns new connection if old connection is not present or closed
+     * Get connection to the DB. Returns new connection if old connection is not present or closed
      * @return connection object
      */
     Connection getConnection() throws SQLException;
 
     /**
-     * Get connection to a DB. Returns new connection even if old still exists (it will be destroyed)
+     * Get connection to the DB. Returns new connection even if old still exists (it will be destroyed)
      * @return connection object
      */
     Connection newConnection() throws SQLException;
 
     /**
-     * Break connection to a DB.
+     * Break connection to the DB.
      * @throws SQLException if connection close throws SQLException
      */
     void breakConnection() throws SQLException;
