50bb9b2b0b6594cb2d773a98355bbedfd0fe884e
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-02-11 01:26:15 +0400

    [wlib-log] Debug: improve debug logging
    I do like sending gigabytes of crap as a debugging messages

--- wlib-log/src/main/java/ru/whbex/lib/log/Debug.java
+++ wlib-log/src/main/java/ru/whbex/lib/log/Debug.java
@@ -6,14 +6,42 @@ public class Debug {
     // May add runtime changing in the future
     // Consider using DEBUG level with SLF4J?
     private static boolean DEBUG = Boolean.parseBoolean(System.getProperty("whbex.debug"));
+
+    /**
+     * Simple debug print
+     * @param m Base message
+     * @param args args
+     */
     public static void print(String m, Object... args){
         if(!DEBUG)
             return;
-        LogContext.getLogger().info(StringUtils.simpleformat("DBG({0}): {1}",
-                Thread.currentThread().getStackTrace()[2].getFileName() +
-                        ':' +
-                        Thread.currentThread().getStackTrace()[2].getLineNumber(),
-                StringUtils.simpleformat(m, args)));
+        LogContext.getLogger().info("Debug message >> " + StringUtils.simpleformat(m, args));
+    }
+
+    /**
+     * Tagged debug print
+     * @param tag Tag
+     * @param m Base message
+     * @param args args
+     */
+    public static void tprint(String tag, String m, Object... args){
+        if(!DEBUG)
+            return;
+        LogContext.getLogger().info("Debug message [" + tag + "] >> " + StringUtils.simpleformat(m, args));
+    }
+
+    /**
+     * Stacktrace debug print (contains sender file name and line number where debug print was issued)
+     * @param m Base message
+     * @param args args
+     */
+    public static void lprint(String m, Object... args){
+        if(!DEBUG)
+            return;
+        final StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+        LogContext.getLogger().info("Debug message (from: " +
+                stackTrace[2].getFileName() + ":" + stackTrace[2].getLineNumber() + " >> ",
+                StringUtils.simpleformat(m, args));
     }
     public static void dbg_printStacktrace(Throwable t){
         if(DEBUG)
